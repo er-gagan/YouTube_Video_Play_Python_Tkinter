95892a0793e4d9a67fefa4fcccbe19f6e83a1fae
Gagan Aggarwal <62302397+pythoniseasy-hub@users.noreply.github.com>
2020-08-01 15:54:24 +0530

    bug fix pause play song
    bug fix pause play song and also design frontend and backend

--- Search-and-Play-YouTube-Video/Search and Play YouTube Video/main.py
+++ Search-and-Play-YouTube-Video/Search and Play YouTube Video/main.py
@@ -22,20 +22,25 @@ SearchVar = StringVar()
 os.system("cls")
 
 def Search():
-    def PlayAudio(argument):
-        vidlink = argument
-            
+    def PlayAudio(vidlink,title,duration):
         audio_root = Toplevel()
-        b1 = Button(audio_root,text="Play",command=lambda: Audio(vidlink,b1,b2,b3))
-        b1.place(x=2,y=2)
+        audio_root.geometry('500x200')
+        photo = PhotoImage(file="./logo.png")
+        audio_root.iconphoto(False,photo)
+        audio_root.resizable(0,0)
+        audio_title = f'''{title[0:60]}\n{title[60:100]}..\n\nSong Duration: {duration}'''
+        Label(audio_root,text=audio_title,font=("Arial",12)).place(x=10,y=10)
         
-        b2 = Button(audio_root,text="Pause/Play",command=PAUSE)
+        b1 = Button(audio_root,text="Play",font=("ArialBlack",20),width=8,bd=5,command=lambda: Audio(vidlink,b1,b2,b3))
+        b1.place(x=10,y=120)
+        
+        b2 = Button(audio_root,text="Pause/Play",font=("ArialBlack",20),width=9,bd=5,command=PAUSE)
         b2['state'] = DISABLED
-        b2.place(x=2,y=30)
+        b2.place(x=167,y=120)
         
-        b3 = Button(audio_root,text="Stop",command=lambda: STOP(b1,b2,b3))
+        b3 = Button(audio_root,text="Stop",font=("ArialBlack",20),width=8,bd=5,command=lambda: STOP(b1,b2,b3))
         b3['state'] = DISABLED
-        b3.place(x=2,y=60)
+        b3.place(x=340,y=120)
         def on_closing():
             if askokcancel("Quit", "Do you want to quit?"):
                 STOP(b1,b2,b3)
@@ -43,20 +48,26 @@ def Search():
         audio_root.protocol("WM_DELETE_WINDOW", on_closing)
         audio_root.mainloop()
         
-    def PlayVideo(argument):
-        vidlink = argument
-        
+    def PlayVideo(vidlink,title,duration):
         video_root = Toplevel()
-        b1 = Button(video_root,text="Play",command=lambda: Video(vidlink,b1,b2,b3))
-        b1.place(x=2,y=2)
+        video_root.geometry('500x200')
+        photo = PhotoImage(file="./logo.png")
+        video_root.iconphoto(False,photo)
+        video_root.resizable(0,0)
         
-        b2 = Button(video_root,text="Pause/Play",command=PAUSE)
+        audio_title = f'''{title[0:60]}\n{title[60:100]}..\n\nSong Duration: {duration}'''
+        Label(video_root,text=audio_title,font=("Arial",12)).place(x=10,y=10)
+        
+        b1 = Button(video_root,text="Play",font=("ArialBlack",20),width=8,bd=5,command=lambda: Video(vidlink,b1,b2,b3))
+        b1.place(x=10,y=120)
+        
+        b2 = Button(video_root,text="Pause/Play",font=("ArialBlack",20),width=9,bd=5,command=PAUSE)
         b2['state'] = DISABLED
-        b2.place(x=2,y=30)
+        b2.place(x=167,y=120)
         
-        b3 = Button(video_root,text="Stop",command=lambda: STOP(b1,b2,b3))
+        b3 = Button(video_root,text="Stop",font=("ArialBlack",20),width=8,bd=5,command=lambda: STOP(b1,b2,b3))
         b3['state'] = DISABLED
-        b3.place(x=2,y=60)
+        b3.place(x=340,y=120)
         
         def on_closing():
             if askokcancel("Quit", "Do you want to quit?"):
@@ -85,9 +96,8 @@ def Search():
     Label(root,text=Channel[0][0:25:]+" | ",font=("ArialBold",12)).place(x=210,y=250)
     Label(root,text=Views[0],font=("ArialBold",12)).place(x=400,y=250)
     Label(root,text="Video Length: "+Duration[0],font=("ArialBold",12)).place(x=210,y=275)
-    argument1 = SearchData[1][0]
-    Button(root,text="Play Audio",font=("ArialBlack",15),bd=5,command=lambda: PlayAudio(argument1)).place(x=620,y=240)
-    Button(root,text="Play Video",font=("ArialBlack",15),bd=5,command=lambda: PlayVideo(argument1)).place(x=780,y=240)
+    Button(root,text="Play Audio",font=("ArialBlack",15),bd=5,command=lambda: PlayAudio(SearchData[1][0],Title[0],Duration[0])).place(x=620,y=240)
+    Button(root,text="Play Video",font=("ArialBlack",15),bd=5,command=lambda: PlayVideo(SearchData[1][0],Title[0],Duration[0])).place(x=780,y=240)
     
     raw_data2 = urllib.request.urlopen(ThumbURL[1]).read()
     im2 = Image.open(io.BytesIO(raw_data2))
@@ -100,9 +110,8 @@ def Search():
     Label(root,text=Channel[1][0:25:]+" | ",font=("ArialBold",12)).place(x=210,y=370)
     Label(root,text=Views[1],font=("ArialBold",12)).place(x=400,y=370)
     Label(root,text="Video Length: "+Duration[1],font=("ArialBold",12)).place(x=210,y=395)
-    argument2 = SearchData[1][1]
-    Button(root,text="Play Audio",font=("ArialBlack",15),bd=5,command=lambda: PlayAudio(argument2)).place(x=620,y=360)
-    Button(root,text="Play Video",font=("ArialBlack",15),bd=5,command=lambda: PlayVideo(argument2)).place(x=780,y=360)
+    Button(root,text="Play Audio",font=("ArialBlack",15),bd=5,command=lambda: PlayAudio(SearchData[1][1],Title[1],Duration[1])).place(x=620,y=360)
+    Button(root,text="Play Video",font=("ArialBlack",15),bd=5,command=lambda: PlayVideo(SearchData[1][1],Title[1],Duration[1])).place(x=780,y=360)
     
     raw_data3 = urllib.request.urlopen(ThumbURL[2]).read()
     im3 = Image.open(io.BytesIO(raw_data3))
@@ -115,15 +124,13 @@ def Search():
     Label(root,text=Channel[2][0:25:]+" | ",font=("ArialBold",12)).place(x=210,y=490)
     Label(root,text=Views[2],font=("ArialBold",12)).place(x=400,y=490)
     Label(root,text="Video Length: "+Duration[2],font=("ArialBold",12)).place(x=210,y=515)
-    argument3 = SearchData[1][2]
-    Button(root,text="Play Audio",font=("ArialBlack",15),bd=5,command=lambda: PlayAudio(argument3)).place(x=620,y=480)
-    Button(root,text="Play Video",font=("ArialBlack",15),bd=5,command=lambda: PlayVideo(argument3)).place(x=780,y=480)
+    Button(root,text="Play Audio",font=("ArialBlack",15),bd=5,command=lambda: PlayAudio(SearchData[1][2],Title[2],Duration[2])).place(x=620,y=480)
+    Button(root,text="Play Video",font=("ArialBlack",15),bd=5,command=lambda: PlayVideo(SearchData[1][2],Title[2],Duration[2])).place(x=780,y=480)
     root.mainloop()
 
 Label(root,text="Search Keywords:",font=("ArialBlack",15)).place(x=50,y=90)
 e1 = Entry(root,font=("ArialBlack",20),width=50,bd=5,textvariable=SearchVar)
 e1.focus()
-SearchVar.set("Laung Lachi")
 e1.place(x=50,y=120)
 Button(root,text="Search",font=("ArialBlack",20),bd=5,command=Search).place(x=840,y=110)
 
--- Search-and-Play-YouTube-Video/Search and Play YouTube Video/text.py
+++
@@ -1,9 +0,0 @@
-from tkinter import *
-root=Tk()
-def msg():
-    print("Hello")
-    b1["state"] = DISABLED
-b1 = Button(root,text="Click",command=msg)
-b1.pack()
-
-root.mainloop()
